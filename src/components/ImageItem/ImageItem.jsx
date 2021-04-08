5a34c032a317868fc72376d3565e489ead848d01
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-04-08 16:17:48 -0500

    added imagelist imageitem

---
+++ src/components/ImageItem/ImageItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageItem = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ImageItem
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -8,6 +8,8 @@ const Search = () => {
 
     const [ query, setQuery ] = useState( '' );
 
+    const [ category, setCategory ] = useState( '' );
+
     const [ urls, setUrls ] = useState( [] );
     const handleChange = event => {
         setQuery( event.target.value );
@@ -26,12 +28,30 @@ const Search = () => {
             })
     }
 
+    // const addToFavs = () => {
+    //     console.log( 'in addToFavs' );
+    //     axios.post( '/api/favorite' )
+    // }
+
     return (
         <div>
             <input onChange = {handleChange} value = {query} type = 'text' placeholder = 'Type of Gif You Haz To Find'></input>
             <button onClick = {() => getGifs()}>Search 4 Ya Gif</button>
             <section>
-                { urls.map( ( image, index ) => { return <div key = { index } ><img src = {image.images.original.url} /><button>Fav Me Plz</button></div> } ) }
+                { urls.map( ( image, index ) => 
+                { return 
+                    <div key = { index } >
+                        <img src = {image.images.original.url} />
+                        <select onChange={(event) => setCategory(event.target.value)} className="categories">
+                            <option value="funny">Funny</option>
+                            <option value="cohort">Cohort</option>
+                            <option value="cartoon">Cartoon</option>
+                            <option value="nfsw">NSFW</option>
+                            <option value="meme">Meme</option>
+                        </select>
+                        { category ? <button onClick = {() => addToFavs()}>Fav Me Plz</button> : '' }
+                    </div> 
+                })}
             </section>
         </div>
     )
