98632100715465d5092038517516d6d3528282d9
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-04-08 13:39:36 -0500

    deleted extra router declaration

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -4,8 +4,6 @@ const pool = require('../modules/pool');
 const axios = require( 'axios' );
 require('dotenv').config();
 
-const router = express.Router();
-
 router.get('/', (req, res) => {
     console.log( 'in /api/giphy GET', req.query)
     axios.get(`http://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${req.query.searchQuery}`)
