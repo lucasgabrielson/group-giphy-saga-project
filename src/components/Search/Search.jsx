5120cc82fb78ef76827e81e16346a56115988494
lucasgabrielson <lucas.c.gabrielson@gmail.com>
2021-04-08 10:54:43 -0500

    created input & button

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+const Search = () => {
+    const dispatch = useDispatch();
+
+    const [ query, setQuery ] = useState('');
+
+    const handleChange = event => {
+        setQuery( event.target.value );
+    }
+
+    const getGifs = () => {
+        dispatch( { type: 'SET_ELEMENTS', payload: [] } );
+    }
+
+    return (
+        <div>
+            <input onChange = {handleChange} value = {query} type = 'text' placeholder = 'Type of Gif You Haz To Find'></input>
+            <button onClick = {() => getGifs()}>Search 4 Ya Gif</button>
+        </div>
+    )
+}
+
+export default Search
